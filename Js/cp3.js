708f1b8abb0cc72b51544b59f96ac5d0e99ca150
LuizFFarias <rm99519@fiap.com.br>
2023-05-21 18:38:46 -0300

    cp3.js alterado

---
+++ Js/cp3.js
@@ -0,0 +1,27 @@
+const inputNome = document.querySelector("#idNome");
+const inputSegNome = document.forms["formulario"]["txtSegNome"].value;
+const inputEmail = document.forms["formulario"]["txtEmail"].value;
+const inputSenha = document.forms["formulario"]["txtSenha"].value;
+const inputConfirmSenha = document.forms["formulario"]["txtConfirmSenha"].value;
+const entrada = document.forms["entrada"];
+
+btnEntrar.addEventListener("")
+if(inputEmail.length <= 5){
+    alert("O email tem que ter no mínimo 5 caracteres!")
+}
+
+if(inputNome.length <= 5){
+    alert("O nome tem que ter no mínimo 5 caracteres!")
+}
+
+if(inputSegNome.length <= 5){
+    alert("O segundo nome tem que ter no mínimo 5 caracteres!")
+}
+
+if(inputSenha.length <= 6 || inputSenha >= 8 ){
+    alert("A tem que ter no mínimo 5 caracteres e no máximo 8 caracteres!")
+}
+
+if(inputConfirmSenha.length <= 5 || inputConfirmSenha >= 8){
+    alert("O email tem que ter no mínimo 5 caracteres e no máximo 8 caracters!")
+}
